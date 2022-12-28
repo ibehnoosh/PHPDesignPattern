bbe33b84840584cf4638abf9be2b390bcc9139dc
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-28 12:28:31 +0330

    Bridge

--- S1_Adaptor/wild.php
+++ S1_Adaptor/wild.php
@@ -29,12 +29,10 @@ class WildDog
     {
     }
 }
-
 // Adapter around wild dog to make it compatible with our game
 class WildDogAdapter implements Lion
 {
     protected $dog;
-
     public function __construct(WildDog $dog)
     {
         $this->dog = $dog;
