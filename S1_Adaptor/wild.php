1a18b0fa907f92f2c5dbd44db27fc22844c661a1
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-28 12:18:44 +0330

    Adaptor

---
+++ S1_Adaptor/wild.php
@@ -0,0 +1,53 @@
+<?php
+interface Lion
+{
+    public function roar();
+}
+class AfricanLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+class AsianLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+class Hunter
+{
+    public function hunt(Lion $lion)
+    {
+        $lion->roar();
+    }
+}
+// This needs to be added to the game
+class WildDog
+{
+    public function bark()
+    {
+    }
+}
+
+// Adapter around wild dog to make it compatible with our game
+class WildDogAdapter implements Lion
+{
+    protected $dog;
+
+    public function __construct(WildDog $dog)
+    {
+        $this->dog = $dog;
+    }
+
+    public function roar()
+    {
+        $this->dog->bark();
+    }
+}
+
+$wildDog = new WildDog();
+$wildDogAdapter = new WildDogAdapter($wildDog);
+
+$hunter = new Hunter();
+$hunter->hunt($wildDogAdapter);
