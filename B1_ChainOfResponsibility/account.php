577b6ca1b76a8f853ee9c009061dd6a937f9eab6
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2023-01-01 12:22:47 +0330

    Chain Of Responsibility Example

--- B1_ChainOfResponsibility/account.php
+++ B1_ChainOfResponsibility/account.php
@@ -6,6 +6,12 @@ abstract class Account
     protected $successor;
     protected $balance;
 
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+
     public function setNext(Account $account)
     {
         $this->successor = $account;
@@ -31,41 +37,15 @@ abstract class Account
 
 class Bank extends Account
 {
-    protected $balance;
-
-    public function __construct(float $balance)
-    {
-        $this->balance = $balance;
-    }
 }
 
 class Paypal extends Account
 {
-    protected $balance;
-
-    public function __construct(float $balance)
-    {
-        $this->balance = $balance;
-    }
 }
 
 class Bitcoin extends Account
 {
-    protected $balance;
-
-    public function __construct(float $balance)
-    {
-        $this->balance = $balance;
-    }
 }
-
-
-// Let's prepare a chain like below
-//      $bank->$paypal->$bitcoin
-//
-// First priority bank
-//      If bank can't pay then paypal
-//      If paypal can't pay then bit coin
 
 $bank = new Bank(100);          // Bank with balance 100
 $paypal = new Paypal(200);      // Paypal with balance 200
@@ -73,12 +53,4 @@ $bitcoin = new Bitcoin(300);    // Bitcoin with balance 300
 
 $bank->setNext($paypal);
 $paypal->setNext($bitcoin);
-
-// Let's try to pay using the first priority i.e. bank
 $bank->pay(259);
-
-// Output will be
-// ==============
-// Cannot pay using bank. Proceeding ..
-// Cannot pay using paypal. Proceeding ..:
-// Paid 259 using Bitcoin!
